1670d659b0061aeec9cf48c811824e206535262a
Nahid-Hassan <nahid.cseru@gmail.com>
2019-05-09 17:08:49 +0600

    using istringstream to separate the word

---
+++ Online Judge Problem Solving/ Decoding the message - 11220 .cpp
@@ -0,0 +1,41 @@
+//Author: nahid.cseru@gmail.com
+#include <bits/stdc++.h>
+using namespace std;
+
+#define input_file() freopen("in.txt", "rt", stdin);
+int input() {int n; cin >> n; return n;}
+
+int main() {
+    //input_file();
+    int testCase = input();
+    string s, s1;
+    int casen = 0;
+
+    cin.ignore();
+    getline(cin, s);
+
+    while(testCase--) {
+        cout << "Case #" << ++casen << ":" << endl;
+
+        while(getline(cin, s)) {
+            if(s == "\n" || s.size() == 0) break;
+            istringstream is(s);
+
+            int idx = 0;
+            while(is >> s1) {
+                if(s1.size() >= idx + 1) {
+                    cout << s1[idx];
+                    idx++;
+                } else {
+                    continue;
+                }
+            }
+            cout << endl;
+        }
+        if (testCase > 0) {
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
