f263e27300a6a272d3ba6f369e346d803a8eeaa9
Nahid-Hassan <nahid.cseru@gmail.com>
2019-05-11 11:52:32 +0600

    Empty commit

--- Online Judge Problem Solving/Test.cpp
+++ Online Judge Problem Solving/Test.cpp
@@ -1,27 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define input_file() {freopen("in.txt", "rt", stdin);}
+
 int main() {
-    string s;
+    //input_file();
     
-    getline(cin, s);
-    getline(cin, s);
-
-    while(s[0] == '|') {
-        unsigned int m = 0;
-        unsigned int bit = 128;
-
-        for(int i = 0; i < s.size(); i++) {
-            if(s[i] == ' ') {
-                bit >>= 1;
-            } else if(s[i] == 'o') {
-                m += bit;
-                bit >>= 1;
-            }
-        }
-        printf("%c", m);
-        getline(cin, s);
-    }
-
     return 0;
 }
