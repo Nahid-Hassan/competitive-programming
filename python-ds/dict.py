22abc1343e17fbb6e3d6220fb7d73d1692a521a6
Nahid-Hassan <nahid.cseru@gmail.com>
2022-06-24 11:53:41 +0600

    python-ds - dict

---
+++ python-ds/dict.py
@@ -0,0 +1,15 @@
+mp = {"name": "nahid", 'age': 32, 'dept': 'cse'} # val: key
+
+# mp.items() -> key, value
+# mp.keys() -> keys
+# mp.values() -> values
+# enumerate()
+
+vals = mp.values()
+keys = mp.keys()
+
+print(vals)
+print(keys)
+
+val = mp['name']
+print(val)
